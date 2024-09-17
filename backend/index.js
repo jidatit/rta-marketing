3816cfefbbf7b435226cd3b2701ff31bc0d0c22c
Mohammad Nehaal <108192533+Mnehaal2000@users.noreply.github.com>
2024-09-18 01:15:01 +0500

    push2

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ admin.initializeApp({
 });
 
 const app = express();
-app.use(cors({ origin: "https://rta-web.jidatit.uk/" }));
+app.use(cors({ origin: "https://rta-web.jidatit.uk/AdminLayout/users" }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
